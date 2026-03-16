136e7152a99a5361c15315f28fe689a660da8202
harkit singh <harkitsinghsran9584@gmail.com>
2026-03-16 12:52:14 +0530

    fix backend

--- Backend/server.js
+++ Backend/server.js
@@ -23,14 +23,12 @@ async function main() {
   await mongoose.connect(process.env.DB_URL);
 }
 
-app.use(cors());
-
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", process.env.CLIENT_URL],
-//     credentials: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
